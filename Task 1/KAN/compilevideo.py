4a1911fcb1e41959d1ec48dd516fb8079d937eb1
OpexRah <dougisrave@gmail.com>
2024-05-31 21:18:22 +0530

    fix: fps changed to 120

--- Task 1/KAN/compilevideo.py
+++ Task 1/KAN/compilevideo.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-def images_to_video(image_folder, video_name, fps=60):
+def images_to_video(image_folder, video_name, fps=120):
     images = [img for img in os.listdir(image_folder) if img.endswith(".jpg") or img.endswith(".png")]
     images.sort()  # Ensure the images are in the correct order
     print(images)
@@ -29,7 +29,3 @@ def images_to_video(image_folder, video_name, fps=60):
     
     print(f'Video saved as {os.path.join(image_folder, video_name)}')
 
-# Usage
-image_folder = 'Task 1\KAN\Visualisation'
-video_name = 'expx_2.mp4'
-images_to_video(image_folder, video_name)
