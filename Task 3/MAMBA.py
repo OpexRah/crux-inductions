098a628756926d1a194eb071e69cd2ef80c12d45
OpexRah <dougisrave@gmail.com>
2024-06-06 20:08:56 +0530

    feat: add call and ssm method with skip connection

--- Task 3/MAMBA.py
+++ Task 3/MAMBA.py
@@ -1,9 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow_datasets as tfds
 from tensorflow import keras
-from tensorflow.keras import layers, Model
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import layers
 from einops import rearrange, repeat
 import math
 
@@ -27,4 +25,63 @@ class MambaBlock(layers.Layer):
         self.out_projection = layers.Dense(model_input_dims,input_shape=(self.model_internal_dim,),use_bias=False)
 
     def call(self, x):
-        pass
+        (batch_size, seq_len, dimension) = x.shape
+
+        x_and_result = self.in_projection(x)
+        (x, result) = tf.split(x_and_result, [self.model_internal_dim, self.model_internal_dim], axis=-1)
+
+        x = rearrange(x, 'b l d_in -> b d_in l')
+        x = self.conv1d(x)[:, :, :seq_len]
+        x = rearrange(x, 'b d_in l -> b l d_in')
+
+        x = tf.nn.swish(x)
+        y = self.ssm(x)
+        y = y * tf.nn.swish(result)
+        return self.out_projection(y)
+    
+    def ssm(self, x):
+        (d_in, n) = self.A_log.shape
+
+
+        A = -tf.exp(tf.cast(self.A_log, tf.float32)) 
+        D = tf.cast(self.D, tf.float32)
+
+        x_dbl = self.x_projection(x) 
+
+        (delta, B, C) = tf.split(
+                x_dbl,
+                num_or_size_splits=[self.delta_t_rank, n, n],
+                axis=-1) 
+
+        delta = tf.nn.softplus(self.delta_t_projection(delta)) 
+
+        # selective scan process
+        
+        dA = tf.einsum('bld,dn->bldn', delta, A)
+        dB_u = tf.einsum('bld,bld,bln->bldn', delta, x, B)
+
+        dA_cumsum = tf.pad(
+            dA[:, 1:], [[0, 0], [1, 1], [0, 0], [0, 0]])[:, 1:, :, :]
+
+        dA_cumsum = tf.reverse(dA_cumsum, axis=[1]) 
+        dA_cumsum = tf.math.cumsum(dA_cumsum, axis=1)
+
+        dA_cumsum = tf.exp(dA_cumsum)
+        dA_cumsum = tf.reverse(dA_cumsum, axis=[1]) 
+
+        x_ss = dB_u * dA_cumsum
+        # 1e-12 to avoid division by 0
+        x_ss = tf.math.cumsum(x_ss, axis=1)/(dA_cumsum + 1e-12)
+
+        y = tf.einsum('bldn,bln->bld', x_ss, C)
+
+        return y + x * D
+    
+class SkipConnect(layers.Layer):
+    def __init__(self,model_input_dims,model_states):
+        super(SkipConnect, self).__init__()
+        self.mixer = MambaBlock(model_input_dims,model_states)
+        self.norm = layers.LayerNormalization(epsilon=1e-5)
+
+    def call(self, x):
+        return self.mixer(self.norm(x)) + x
